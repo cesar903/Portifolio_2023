0061124de3dd881e292302512306fd895e30dafd
CESAR AUGUSTO <59238654+cesar903@users.noreply.github.com>
2023-04-03 13:44:08 -0300

    first commit

---
+++ Portifolio_Cesar2023/Portifolio/src/app/app.module.ts
@@ -0,0 +1,38 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppComponent } from './app.component';
+import { ArticleComponent } from './components/article/article.component';
+import { AboutComponent } from './components/about/about.component';
+import { RouterModule, Routes } from "@angular/router";
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { ContactsComponent } from './components/contacts/contacts.component';
+import { FooterComponent } from './components/footer/footer.component';
+import {TranslateModule} from '@ngx-translate/core';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ArticleComponent,
+    AboutComponent,
+    ProjectsComponent,
+    ContactsComponent,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule,
+    ReactiveFormsModule,
+    NgbModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    FormsModule,
+    TranslateModule.forRoot({defaultLanguage: 'en'})
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
